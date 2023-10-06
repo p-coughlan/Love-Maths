8bf7d564765511c218652b30b2ca4da8997d949e
p-coughlan <patrickcoughlan@yahoo.co.uk>
2023-10-06 19:13:08 +0100

    set up structure of game functions in script.js

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
